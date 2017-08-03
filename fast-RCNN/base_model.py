c714e6c4d2ea41183639d9dbbead6632793d5a1a
Sreejith <sreejith2904@gmail.com>
2017-08-03 14:48:46 -0400

    base model class for loading vgg16

--- fast-RCNN/base_model.py
+++ fast-RCNN/base_model.py
@@ -1,9 +1,12 @@
-import create_data
 
 __author__ = "Sreejith Sreekumar"
 __email__ = "sreekumar.s@husky.neu.edu"
 __version__ = "0.0.1"
 
-coco, dataset = create_data.prepare_train_coco_data()
 
-model = ObjectDetector()
+class BaseModel(object):
+    
+    def __init__():
+        self.num_class
+        
+
